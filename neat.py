c1e4a801b2860b9dc6e2d715aea64bb8d5f83104
Justin Reese <justaddcoffee+github@gmail.com>
2020-10-14 14:29:53 -0700

    Add run method to neat.py

--- neat.py
+++ neat.py
@@ -5,3 +5,19 @@ import click
 def cli():
     pass
 
+
+@cli.command()
+@click.option("yaml_file", "-y", required=True, default="neat.yaml",
+              type=click.Path(exists=True))
+def run(*args, **kwargs) -> None:
+    """Run a NEAT pipeline using the given YAML file [neat.yaml]
+
+    Args:
+        yaml_file: Specify the YAML file containing a list of datasets to download.
+
+    Returns:
+        None.
+
+    """
+    print("hello world")
+    return None
