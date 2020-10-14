3615f251f4ebb01b515a7af0e86cef0ec3c7de66
Justin Reese <justaddcoffee+github@gmail.com>
2020-10-14 16:19:32 -0700

    Add code to load graph using yaml params

--- neat/embeddings.py
+++ neat/embeddings.py
@@ -1,4 +1,10 @@
+import os
+
+from ensmallen_graph import EnsmallenGraph
+import tempfile
 
 
 def make_embeddings(yaml_args: dict) -> None:
-    pass
+    sorted_edges = os.path.join(tempfile.mkdtemp(), 'sorted_edges.tsv')
+    graph = EnsmallenGraph.from_unsorted_csv(**yaml_args['embeddings']['ensmallen_params'])
+    return None
