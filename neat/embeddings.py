c6d898ac2912d112b2e5f2e8a3d9e7ec4b844595
Justin Reese <justaddcoffee+github@gmail.com>
2020-10-15 13:41:54 -0700

    Should work for CBOW now too

--- neat/embeddings.py
+++ neat/embeddings.py
@@ -1,7 +1,7 @@
 import re
 
 from ensmallen_graph import EnsmallenGraph
-from embiggen import Node2VecSequence, SkipGram
+from embiggen import Node2VecSequence, SkipGram, CBOW
 from tensorflow.keras.optimizers import Nadam
 from tensorflow.keras.callbacks import EarlyStopping
 
@@ -15,21 +15,21 @@ def make_embeddings(config: dict) -> None:
         es = EarlyStopping(**config['embiggen_params']['seq_params']['early_stopping'])
         callbacks = [es]
 
-    if re.search('skipgram', config['embiggen_params']['model']):
-        lr = Nadam(config['embiggen_params']['optimizer']['learning_rate'])
-        model = SkipGram(
-            vocabulary_size=graph.get_nodes_number(),
-            optimizer=lr,
-            **config['embiggen_params']['node2vec_params']
-        )
-        model.summary()
+    lr = Nadam(config['embiggen_params']['optimizer']['learning_rate'])
+    if re.search('skipgram', config['embiggen_params']['model'], re.IGNORECASE):
+        model = SkipGram(vocabulary_size=graph.get_nodes_number(), optimizer=lr,
+                         **config['embiggen_params']['node2vec_params'])
+    elif re.search('CBOW', config['embiggen_params']['model'], re.IGNORECASE):
+        model = CBOW(vocabulary_size=graph.get_nodes_number(), optimizer=lr,
+                     **config['embiggen_params']['node2vec_params'])
+    model.summary()
 
-        history = model.fit(
-            graph_sequence,
-            steps_per_epoch=graph_sequence.steps_per_epoch,
-            callbacks=callbacks,
-            epochs=config['embiggen_params']['epochs']
-        )
+    history = model.fit(
+        graph_sequence,
+        steps_per_epoch=graph_sequence.steps_per_epoch,
+        callbacks=callbacks,
+        epochs=config['embiggen_params']['epochs']
+    )
 
     return None
 
