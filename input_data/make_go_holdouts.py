866553e327a0af01176632bf6a997b5ff54ed915
Justin Reese <justaddcoffee+github@gmail.com>
2020-11-09 10:23:56 -0800

    Fix neg to use reduced graph, dump nodes for reduced graph

--- input_data/make_go_holdouts.py
+++ input_data/make_go_holdouts.py
@@ -21,12 +21,13 @@ pos_training, pos_validation = reduced_graph.connected_holdout(
     random_state=seed)
 
 # make negative graph
-neg_training, neg_validation = graph.sample_negatives(
+neg_training, neg_validation = reduced_graph.sample_negatives(
    random_state=seed,
    only_from_same_component=True,
    negatives_number=graph.get_edges_number(),
 ).random_holdout(random_state=seed, train_size=train_percentage)
 
+reduced_graph.dump_nodes("go_nodes_training.tsv")
 pos_training.dump_edges("go_edges_training.tsv")
 pos_validation.dump_edges("go_edges_validation.tsv")
 neg_training.dump_edges("go_edges_neg_training.tsv")
