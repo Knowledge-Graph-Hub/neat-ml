946f2c6e464f123714312bf66fe31d138018742f
Justin Reese <justaddcoffee+github@gmail.com>
2020-11-12 18:30:27 -0800

    simplify file names

--- input_data/make_go_holdouts.py
+++ input_data/make_go_holdouts.py
@@ -47,11 +47,12 @@ for edges in (
        negatives_number=graph.get_edges_number(),
     ).random_holdout(random_state=seed, train_size=train_percentage)
 
-    reduced_graph.dump_nodes(os.path.join(edges_string, f"go_nodes_training_{edges_string}.tsv"))
+    reduced_graph.dump_nodes(os.path.join(edges_string, f"go_nodes_training.tsv"))
 
-    pos_training.dump_edges(os.path.join(edges_string, f"go_edges_training_{edges_string}.tsv"))
-    pos_validation.dump_edges(os.path.join(edges_string, f"go_edges_validation_{edges_string}.tsv"))
-    neg_training.dump_edges(os.path.join(edges_string, f"go_edges_neg_training_{edges_string}.tsv"))
-    neg_validation.dump_edges(os.path.join(edges_string, f"go_edges_neg_validation_{edges_string}.tsv"))
-    os.system(f"sed -i 's/$/\tlabel/' {edges_string}/go_edges_neg_training_{edges_string}.tsv")
-    os.system(f"sed -i 's/$/\tlabel/' go_edges_neg_validation_{edges_string}.tsv")
+    pos_training.dump_edges(os.path.join(edges_string, f"go_edges_training.tsv"))
+    pos_validation.dump_edges(os.path.join(edges_string, f"go_edges_validation.tsv"))
+    neg_training.dump_edges(os.path.join(edges_string, f"go_edges_neg_training.tsv"))
+    neg_validation.dump_edges(os.path.join(edges_string, f"go_edges_neg_validation.tsv"))
+
+    os.system(f"sed -i '.bak' 's/$/\\tlabel/' {edges_string}/go_edges_neg_training.tsv")
+    os.system(f"sed -i '.bak' 's/$/\\tlabel/' {edges_string}/go_edges_neg_validation.tsv")
