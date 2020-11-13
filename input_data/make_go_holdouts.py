d8c14d9e77b8616f17e3bfa49cce2b6414cfd096
Justin Reese <justaddcoffee+github@gmail.com>
2020-11-12 18:08:26 -0800

    Better make_go_holdouts.py, also with different edge types

--- input_data/make_go_holdouts.py
+++ input_data/make_go_holdouts.py
@@ -3,37 +3,55 @@ import os
 from ensmallen_graph import EnsmallenGraph
 train_percentage = 0.8
 seed = 42
-graph = EnsmallenGraph.from_unsorted_csv(
-    # name="go-json",
-    edge_path="go_edges.tsv",
-    sources_column="subject",
-    destinations_column="object",
-    edge_types_column='edge_label',
-    directed=False,
-    node_path="go_nodes.tsv",
-    nodes_column='id',
-    node_types_column='category',
-    default_node_type='biolink:NamedThing'
-)
 
-reduced_graph = graph.remove(singletons=True)
-pos_training, pos_validation = reduced_graph.connected_holdout(
-    train_size=train_percentage,
-    edge_types=['biolink:subclass_of', 'biolink:part_of'],
-    random_state=seed)
+go_json_file = "go.json"
+go_edges_file = "go_edges.tsv"
+go_nodes_file = "go_nodes.tsv"
 
-# make negative graph
-neg_training, neg_validation = reduced_graph.sample_negatives(
-   random_state=seed,
-   only_from_same_component=True,
-   negatives_number=graph.get_edges_number(),
-).random_holdout(random_state=seed, train_size=train_percentage)
+if not os.path.exists(go_json_file):
+    os.system("wget http://purl.obolibrary.org/obo/go.json -o go.json")
 
-reduced_graph.dump_nodes("go_nodes_training.tsv")
-pos_training.dump_edges("go_edges_training.tsv")
-pos_validation.dump_edges("go_edges_validation.tsv")
-neg_training.dump_edges("go_edges_neg_training.tsv")
-neg_validation.dump_edges("go_edges_neg_validation.tsv")
+if not os.path.exists(go_edges_file) or not os.path.exists(go_nodes_file):
+    os.system("kgx transform --input-format obojson --output-format tsv --output go go.json")
 
-os.system("sed -i 's/$/\tlabel/' go_edges_neg_training.tsv")
-os.system("sed -i 's/$/\tlabel/' go_edges_neg_validation.tsv")
+for edges in (
+    ['biolink:subclass_of', 'biolink:part_of'],
+    ['biolink:subclass_of'],
+    ['biolink:part_of']
+):
+    edges_string = "_".join(edges)
+    os.makedirs(edges_string, exist_ok=True)
+
+    graph = EnsmallenGraph.from_unsorted_csv(
+        edge_path=go_edges_file,
+        sources_column="subject",
+        destinations_column="object",
+        edge_types_column='edge_label',
+        directed=False,
+        node_path=go_nodes_file,
+        nodes_column='id',
+        node_types_column='category',
+        default_node_type='biolink:NamedThing'
+    )
+
+    reduced_graph = graph.remove(singletons=True)
+    pos_training, pos_validation = reduced_graph.connected_holdout(
+        train_size=train_percentage,
+        edge_types=edges,
+        random_state=seed)
+
+    # make negative graph
+    neg_training, neg_validation = reduced_graph.sample_negatives(
+       random_state=seed,
+       only_from_same_component=True,
+       negatives_number=graph.get_edges_number(),
+    ).random_holdout(random_state=seed, train_size=train_percentage)
+
+    reduced_graph.dump_nodes(os.path.join(edges_string, f"go_nodes_training_{edges_string}.tsv"))
+
+    pos_training.dump_edges(os.path.join(edges_string, f"go_edges_training_{edges_string}.tsv"))
+    pos_validation.dump_edges(os.path.join(edges_string, f"go_edges_validation_{edges_string}.tsv"))
+    neg_training.dump_edges(os.path.join(edges_string, f"go_edges_neg_training_{edges_string}.tsv"))
+    neg_validation.dump_edges(os.path.join(edges_string, f"go_edges_neg_validation_{edges_string}.tsv"))
+    os.system(f"sed -i 's/$/\tlabel/' {edges_string}/go_edges_neg_training_{edges_string}.tsv")
+    os.system(f"sed -i 's/$/\tlabel/' go_edges_neg_validation_{edges_string}.tsv")
