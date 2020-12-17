c2027bff40765ca8cf0e6a14aa6fcc1bd01dd37f
Justin Reese <justaddcoffee@gmail.com>
2020-12-17 10:39:00 -0800

    Add a little test

---
+++ tests/test_graph_embedding.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from neat.graph_embedding.graph_embedding import get_node_data
+import pandas as pd
+
+
+class TestGraphEmbedding(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    def setUp(self) -> None:
+        self.test_node_file = 'tests/resources/test_graphs/test_small_nodes.tsv'
+
+    def test_get_node_data(self):
+        node_data = get_node_data(self.test_node_file)
+        self.assertTrue(isinstance(node_data, pd.DataFrame))
+
+    def test_make_graph_embeddings(self):
+        self.assertTrue(True)
