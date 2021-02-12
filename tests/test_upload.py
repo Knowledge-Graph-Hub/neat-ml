83c63514114a2852e5c34f6e48dab0b74c49da98
Justin Reese <justaddcoffee@gmail.com>
2021-02-11 19:42:53 -0800

    Add simple boto3 test

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 from botocore.exceptions import ClientError
 from neat.upload.upload import upload_dir_to_s3
 from neat.yaml_helper.yaml_helper import YamlHelper
@@ -14,18 +15,12 @@ class TestUpload(TestCase):
     def setUp(self) -> None:
         self.bad_yaml = 'tests/resources/test_bad_upload_info.yaml'
         self.good_yaml = 'tests/resources/test_good_upload_info.yaml'
+        self.good_kwargs = YamlHelper(self.good_yaml).make_upload_args()
 
-    @mock.patch("boto3.client.head_object")
-    @mock.patch("boto3.client.upload_file")
     @mock.patch("boto3.client")
-    def test_upload_dir_to_s3(self, mock_boto_client, mock_upload_file, mock_head_object):
-        mock_upload_file.return_value = {}
-        mock_head_object.return_value = ClientError({'Error':
-                                                     {'Code': '42',
-                                                      'Message': "don't kill bugs"}},
-                                                   "head_object")
-        mock_upload_file.upload_file = {}
-        kwargs = YamlHelper(self.good_yaml).make_upload_args()
-        upload_dir_to_s3(**kwargs)
-        self.assertEqual(mock_boto_client.call_count, 1)
+    def test_upload_dir_to_s3_calls_boto3(self, mock_boto_client):
+        upload_dir_to_s3(**self.good_kwargs)
+        self.assertTrue(mock_boto_client.called)
+
+
 
