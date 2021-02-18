aeb94c6a8e0cd7d4e45a6795191dd1e2acec6c03
Justin Reese <justaddcoffee@gmail.com>
2021-02-17 18:14:14 -0800

    Code to produce holdouts from Ensmallen

---
+++ neat/holdouts/holdouts.py
@@ -0,0 +1,76 @@
+import logging
+import os
+from typing import Dict
+
+from ensmallen_graph import EnsmallenGraph  # type: ignore
+
+
+def make_holdouts(main_graph_args: dict, output_dir: str,
+                  train_size: float, validation: bool, seed=42) -> None:
+    """Prepare positive and negative edges for testing and training (see run.py holdouts
+    command for documentation)
+
+    Args:
+        :param main_graph_args: how to load the main graph (produced by YamlHelper().main_graph_args()
+        :param output_dir:      where should we write out holdout data
+        :param train_fraction:  fraction of edges to emit as training
+        :param validation:      should we make validation edges? [False]
+        :param seed:            random seed [42]
+    Returns:
+        None.
+    """
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Loading graph")
+    graph = EnsmallenGraph.from_unsorted_csv(**main_graph_args)
+
+    # make positive edges
+    logging.info("Making positive edges")
+    pos_train_edges, pos_test_edges = graph.random_holdout(random_state=seed,
+                                                           train_size=train_size)
+    pos_valid_edges = None
+    if validation:
+        pos_valid_edges, pos_test_edges = \
+            pos_test_edges.random_holdout(random_state=seed,
+                                          train_size=0.5)
+
+    # make negative edges
+    logging.info("Making negative edges")
+
+    all_negative_edges = \
+        pos_train_edges.sample_negatives(random_state=seed,
+                                         negatives_number=graph.get_edges_number())
+    neg_train_edges, neg_test_edges = \
+        all_negative_edges.random_holdout(random_state=seed, train_size=train_size)
+    neg_valid_edges = None
+    if validation:
+        neg_test_edges, neg_valid_edges = \
+            neg_test_edges.random_holdout(random_state=seed, train_size=0.5)
+
+    #
+    # write out positive edges
+    #
+    # training:
+    logging.info("Writing out positive edges")
+    pos_train_edges_outfile = os.path.join(output_dir, "pos_train_edges.tsv")
+    pos_train_nodes_outfile = os.path.join(output_dir, "pos_train_nodes.tsv")
+    pos_test_edges_outfile = os.path.join(output_dir, "pos_test_edges.tsv")
+    pos_valid_edges_outfile = os.path.join(output_dir, "pos_valid_edges.tsv")
+
+    pos_train_edges.dump_edges(path=pos_train_edges_outfile)
+    pos_train_edges.dump_nodes(path=pos_train_nodes_outfile)
+    pos_test_edges.dump_edges(path=pos_test_edges_outfile)
+    if validation:
+        pos_valid_edges.dump_edges(path=pos_valid_edges_outfile)
+
+    #
+    # write out negative edges
+    #
+    logging.info("Writing out negative edges")
+    neg_train_edges_outfile = os.path.join(output_dir, "neg_train_edges.tsv")
+    neg_test_edges_outfile = os.path.join(output_dir, "neg_test_edges.tsv")
+    neg_valid_edges_outfile = os.path.join(output_dir, "neg_valid_edges.tsv")
+
+    neg_train_edges.dump_edges(path=neg_train_edges_outfile)
+    neg_test_edges.dump_edges(path=neg_test_edges_outfile)
+    if validation:
+        neg_valid_edges.dump_edges(path=neg_valid_edges_outfile)
---
+++ tests/test_holdouts.py
@@ -0,0 +1,43 @@
+import os
+import tempfile
+from unittest import TestCase
+
+from neat.holdouts.holdouts import make_holdouts
+from neat.yaml_helper.yaml_helper import YamlHelper
+
+
+class TestHoldouts(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_yaml_file = 'tests/resources/test.yaml'
+        cls.yaml_helper = YamlHelper(cls.test_yaml_file)
+        cls.main_graph_args = cls.yaml_helper.main_graph_args()
+        cls.train_fraction = 0.99
+        cls.seed = 21
+        cls.tmp_output_dir = tempfile.TemporaryDirectory()
+        # expensive, do this once:
+        make_holdouts(main_graph_args=cls.main_graph_args,
+                      output_dir=cls.tmp_output_dir.name,
+                      train_size=cls.train_fraction, validation=False, seed=cls.seed)
+
+    def setUp(self) -> None:
+        pass
+
+    def test_output_files(self):
+        self.assertListEqual(os.listdir(self.tmp_output_dir.name),
+                             ['pos_train_edges.tsv', 'neg_train_edges.tsv',
+                              'pos_test_edges.tsv', 'pos_train_nodes.tsv',
+                              'neg_test_edges.tsv'])
+
+    def test_validation_files(self):
+        different_tmpdir = tempfile.TemporaryDirectory()
+        make_holdouts(main_graph_args=self.main_graph_args,
+                      output_dir=different_tmpdir.name,
+                      train_size=self.train_fraction, validation=True, seed=self.seed)
+        self.assertListEqual(os.listdir(different_tmpdir.name),
+                             ['neg_valid_edges.tsv', 'pos_valid_edges.tsv',
+                              'pos_train_edges.tsv', 'neg_train_edges.tsv',
+                              'pos_test_edges.tsv', 'pos_train_nodes.tsv',
+                              'neg_test_edges.tsv'])
+
