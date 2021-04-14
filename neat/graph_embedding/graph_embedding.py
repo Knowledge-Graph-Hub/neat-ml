550b8e9b6af73076d1a2e11a6f882528031917f0
Justin Reese <justaddcoffee@gmail.com>
2021-04-14 16:55:07 -0700

    Passing test to write out embedding history

--- neat/graph_embedding/graph_embedding.py
+++ neat/graph_embedding/graph_embedding.py
@@ -124,6 +124,10 @@ def make_graph_embeddings(main_graph_args: dict,
     ## TODO: deal with GloVe
     history = word2vec_model.fit(graph_sequence, **fit_args)
 
+    if embedding_history_outfile:
+        with open(embedding_history_outfile, 'w') as f:
+            f.write(history.to_json())
+
     these_embeddings = pd.DataFrame(word2vec_model.embedding,
                                     index=graph.get_node_names())
 
