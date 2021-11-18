57a298ebf70034d0a4e9bcfef03e0f271e31d649
Justin Reese <justaddcoffee@gmail.com>
2021-11-17 17:28:02 -0800

    Add tests

--- tests/test_neat.py
+++ tests/test_neat.py
@@ -32,7 +32,7 @@ class TestRun(TestCase):
         result = self.runner.invoke(catch_exceptions=False,
                                     cli=run,
                                     args=['--config',
-                                          'tests/resources/test_good_upload_info.yaml'])
+                                          'tests/resources/test_neat.yaml'])
         self.assertTrue(mock_do_upload.called)
         self.assertTrue(mock_boto3_client.called)
         self.assertEqual(result.exit_code, 0)
@@ -52,7 +52,40 @@ class TestRun(TestCase):
         result = self.runner.invoke(catch_exceptions=False,
                                     cli=run,
                                     args=['--config',
-                                          'tests/resources/test_good_upload_info.yaml'])
-        # self.assertTrue(mock_do_embeddings.called)
-        # self.assertTrue(mock_make_node_embeddings.called)
+                                          'tests/resources/test_neat.yaml'])
+        self.assertEqual(result.exit_code, 0)
+
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_upload")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_classifier")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_tsne")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_embeddings")
+    @mock.patch("neat.visualization.visualization.make_tsne")
+    def test_run_do_embeddings(self, mock_make_tnse,
+                               mock_do_embeddings, mock_do_tsne, mock_do_classifier,
+                               mock_do_upload):
+        mock_do_embeddings.return_value = False
+        mock_do_tsne.return_value = True
+        mock_do_classifier.return_value = False
+        mock_do_upload.return_value = False
+        result = self.runner.invoke(catch_exceptions=False,
+                                    cli=run,
+                                    args=['--config',
+                                          'tests/resources/test_neat.yaml'])
+        self.assertEqual(result.exit_code, 0)
+
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_upload")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_classifier")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_tsne")
+    @mock.patch("neat.yaml_helper.yaml_helper.YamlHelper.do_embeddings")
+    def test_run_do_embeddings(self,
+                               mock_do_embeddings, mock_do_tsne, mock_do_classifier,
+                               mock_do_upload):
+        mock_do_embeddings.return_value = False
+        mock_do_tsne.return_value = False
+        mock_do_classifier.return_value = True
+        mock_do_upload.return_value = False
+        result = self.runner.invoke(catch_exceptions=False,
+                                    cli=run,
+                                    args=['--config',
+                                          'tests/resources/test_neat.yaml'])
         self.assertEqual(result.exit_code, 0)
