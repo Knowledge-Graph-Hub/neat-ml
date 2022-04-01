547a737619741629a9ed53ef5737c9e953883024
caufieldjh <j.harry.caufield@gmail.com>
2022-04-01 14:43:27 -0400

    Add test for downloading compressed file

--- tests/test_yaml_helper.py
+++ tests/test_yaml_helper.py
@@ -192,3 +192,13 @@ class TestYamlHelper(TestCase):
         for this_mock in [mock_open, mock_urlopen, mock_Request]:
             self.assertTrue(this_mock.called)
             self.assertEqual(1, this_mock.call_count)
+
+    @mock.patch('neat.yaml_helper.yaml_helper.Request')
+    @mock.patch('neat.yaml_helper.yaml_helper.urlopen')
+    @mock.patch('neat.yaml_helper.yaml_helper.open')
+    @mock.patch('tarfile.open')
+    def test_download_compressed_file(self, mock_tarfile_open, mock_open, mock_urlopen, mock_Request):
+        download_file("https://someurl.com/file.tar.gz", outfile="file.tar.gz")
+        for this_mock in [mock_tarfile_open, mock_open, mock_urlopen, mock_Request]:
+            self.assertTrue(this_mock.called)
+            self.assertEqual(1, this_mock.call_count)
