2369baed0b54186de557bafc4de5a5a042ffbe5d
caufieldjh <j.harry.caufield@gmail.com>
2022-06-09 12:02:19 -0400

    Add new function to generate full Ensmallen plot set

--- neat/visualization/visualization.py
+++ neat/visualization/visualization.py
@@ -10,6 +10,7 @@ def make_tsne(
         embedding_file: str,
 ) -> None:
     """
+    Generate a simple node tSNE plot.
     :param graph: Ensmallen graph object
     :param tsne_outfile: where to output tSNE file
     :param embedding_file: file containing embeddings (in numpy format) to use to
@@ -21,5 +22,26 @@ def make_tsne(
 
     visualizer = GraphVisualizer(graph)
     figure = visualizer.plot_nodes(visualizer.fit_nodes(node_embeddings))
+    print(figure[1])
+    figure[0].savefig(tsne_outfile)
+
+def make_all_plots(
+        graph: Graph,
+        tsne_outfile: str,
+        embedding_file: str,
+        ) -> None:
+    """
+    Generate the full set of Ensmallen plots.
+    :param graph: Ensmallen graph object
+    :param tsne_outfile: where to output plot image file
+    :param embedding_file: file containing embeddings (in numpy format) to use to
+        generate the tSNE plot
+    :return:
+    """
+
+    node_embeddings = pd.read_csv(embedding_file, index_col=0, header=None)
+
+    visualizer = GraphVisualizer(graph)
+    figure = visualizer.fit_and_plot_all(node_embeddings)
     print(figure[1])
     figure[0].savefig(tsne_outfile)
