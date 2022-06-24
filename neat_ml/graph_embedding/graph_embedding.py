f02a6e3bdc5f5d0a78a49a8547c930c821641d43
caufieldjh <j.harry.caufield@gmail.com>
2022-06-23 22:24:00 -0400

    Re-animate graph embedding tests

--- neat_ml/graph_embedding/graph_embedding.py
+++ neat_ml/graph_embedding/graph_embedding.py
@@ -32,7 +32,7 @@ def make_node_embeddings(
     :param node_embedding_params: Arguments passed to Embiggen.
     :param bert_columns: Columns containing text info to
         use to make embeddings from Bert pretrained embeddings
-    :param bert_pretrained_model: _description_, defaults to 
+    :param bert_pretrained_model: _description_, defaults to
     "allenai/scibert_scivocab_uncased"
     :return: None
     """
