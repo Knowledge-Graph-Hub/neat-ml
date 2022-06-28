d0102e46d48390d963d9a468a66861624e8c5d8d
Harshad Hegde <hegdehb@gmail.com>
2022-06-28 11:38:44 -0500

    flake8-ed

--- neat_ml/link_prediction/mlp_model.py
+++ neat_ml/link_prediction/mlp_model.py
@@ -29,7 +29,8 @@ class MLPModel(Model):
         model_type = config["classifier_type"]
         if not HAVE_TF:
             warn(
-                "Tensorflow not available - specified method may not be accessible!"
+                "Tensorflow not available - specified method \
+                may not be accessible!"
             )
         model_class = self.dynamically_import_class(model_type)
         model_layers = []
