1b5d933fa3e1f7d0464e72e6d9494604ce600144
caufieldjh <j.harry.caufield@gmail.com>
2022-07-28 18:26:42 -0400

    Cleanup

--- neat_ml/link_prediction/grape_model.py
+++ neat_ml/link_prediction/grape_model.py
@@ -31,19 +31,19 @@ class GrapeModel(Model):
             edge_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None):
         """Fit model.
 
-        Imported from the Ensmallen abstract 
+        Imported from the Ensmallen abstract
         edge prediction model class.
         """
         self.is_fit = True
-        return self.model.fit(graph, 
-                                support, 
-                                node_features, 
-                                node_type_features, 
+        return self.model.fit(graph,
+                                support,
+                                node_features,
+                                node_type_features,
                                 edge_features
         )
 
-    def predict(self, 
-            graph: Graph, 
+    def predict(self,
+            graph: Graph,
             support: Optional[Graph] = None,
             node_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
             node_type_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
@@ -51,37 +51,37 @@ class GrapeModel(Model):
             return_predictions_dataframe: bool = False):
         """Predict based on model fitted to graph.
 
-        Imported from the Ensmallen abstract 
+        Imported from the Ensmallen abstract
         edge prediction model class.
         """
-        return self.model.predict(graph, 
-                                support, 
-                                node_features, 
-                                node_type_features, 
-                                edge_features, 
+        return self.model.predict(graph,
+                                support,
+                                node_features,
+                                node_type_features,
+                                edge_features,
                                 return_predictions_dataframe
-        )
+                                )
 
     def predict_proba(self, 
-            graph: Graph, 
-            support: Optional[Graph] = None,
-            node_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
-            node_type_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
-            edge_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
-            return_predictions_dataframe: bool = False):
+                        graph: Graph, 
+                        support: Optional[Graph] = None,
+                        node_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
+                        node_type_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
+                        edge_features: Optional[Union[pd.DataFrame, np.ndarray, List[Union[pd.DataFrame, np.ndarray]]]] = None,
+                        return_predictions_dataframe: bool = False):
         """Predict based on model fitted to graph.
 
         Provides probability values.
-        Imported from the Ensmallen abstract 
+        Imported from the Ensmallen abstract
         edge prediction model class.
         """
-        return self.model.predict_proba(graph, 
-                                support, 
-                                node_features, 
-                                node_type_features, 
-                                edge_features, 
-                                return_predictions_dataframe
-        )
+        return self.model.predict_proba(graph,
+                                        support,
+                                        node_features,
+                                        node_type_features,
+                                        edge_features,
+                                        return_predictions_dataframe
+                                        )
 
     # Grape methods don't currently support loading
     # classifiers, so this is a workaround.
@@ -89,7 +89,7 @@ class GrapeModel(Model):
     def load(self, path: str) -> tuple():  # type: ignore
         """Load the model."""
         print(f"Looking at {path}...")
-    
+
     def save(self) -> None:  # type: ignore
         """Save the model."""
         path = os.path.join(self.outdir, self.config["outfile"])
